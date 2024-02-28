4cbd1d7d506ea2905162f344e14c2e32b50879bb
SayakSarkar02 <sayaksarkar73@gmail.com>
2024-02-28 23:31:52 +0530

    push

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className='w-screen h-screen red-grad flex items-center justify-center'>
       <div className='flex flex-col items-center justify-center h-fit'>
         <Image alt='logo' src={logo} width={500} height={500} className='-my-12'/>
-        <h3 className='text-2xl text-[#DF759C]'>WE make it happen.</h3>
+        <h3 className='text-2xl text-[#DF759C]'>WE make it happen...</h3>
       </div>
     </main>
   )
