dec7ac4f1fee472390cf16fb4f183f6ba0d6ba0a
SayakSarkar02 <sayaksarkar73@gmail.com>
2024-02-29 01:07:47 +0530

    ui initi push

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,77 @@
-import Image from 'next/image'
-import logo from '@/assets/logo.png'
+import Home from '../components/home'
+import { PinContainer } from "../components/3d-pin";
 
-export default function Home() {
+const page = () => {
   return (
-    <main className='w-screen h-screen red-grad flex items-center justify-center'>
-      <div className='flex flex-col items-center justify-center h-fit'>
-        <Image alt='logo' src={logo} width={500} height={500} className='-my-12'/>
-        <h3 className='text-2xl text-[#DF759C]'>WE make it happen...</h3>
+    <div className='red-grad'>
+      <Home/>
+
+
+      <div className='bg-gradient-to-br from-black via-slate-900/60 to-black pt-20 border-x-0 rounded-t-3xl border-white/80 border-2'>
+        <h3 className='text-white text-3xl text-center mb-4'>Our Packages</h3>
+        <h3 className='text-white/50 text-base text-center w-[60%] mx-auto'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit ratione alias assumenda id odio officiis debitis aliquam velit blanditiis deleniti.</h3>
+        <div className='flex flex-col gap-5'>
+          <div className=" my-20 mt-10 w-full flex flex-row items-center justify-center ">
+            <PinContainer
+              className=""
+              containerClassName=""
+              title="Contact us to make your own dapp"
+              href="https://twitter.com/mannupaaji"
+            >
+              <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+                <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+                  Minting dapp
+                </h3>
+                <div className="text-base !m-0 !p-0 font-normal">
+                  <span className="text-slate-500 ">
+                    Dapp to Mint
+                  </span>
+                </div>
+                <div className="flex flex-1 w-full rounded-lg mt-4 red-grad brightness-150" />
+              </div>
+            </PinContainer>
+            <PinContainer
+              className=""
+              containerClassName=""
+              title="Contact us to make your own dapp"
+              href="https://twitter.com/mannupaaji"
+            >
+              <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+                <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+                  Minting dapp
+                </h3>
+                <div className="text-base !m-0 !p-0 font-normal">
+                  <span className="text-slate-500 ">
+                    Dapp to Mint
+                  </span>
+                </div>
+                <div className="flex flex-1 w-full rounded-lg mt-4 red-grad brightness-150" />
+              </div>
+            </PinContainer>
+            <PinContainer
+              className=""
+              containerClassName=""
+              title="Contact us to make your own dapp"
+              href="https://twitter.com/mannupaaji"
+            >
+              <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+                <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+                  Minting dapp
+                </h3>
+                <div className="text-base !m-0 !p-0 font-normal">
+                  <span className="text-slate-500 ">
+                    Dapp to Mint
+                  </span>
+                </div>
+                <div className="flex flex-1 w-full rounded-lg mt-4 red-grad brightness-150" />
+              </div>
+            </PinContainer>
+          </div>
+        </div>
+
       </div>
-    </main>
+    </div>
   )
 }
+
+export default page
---
+++ components/home.jsx
@@ -0,0 +1,42 @@
+"use client"
+import Image from 'next/image'
+
+import { useScroll, useTransform } from "framer-motion";
+import GoogleGeminiEffect from "../components/google-gemini-effect";
+import { useRef } from 'react';
+
+export default function Home() {
+
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const pathLengthFirst = useTransform(scrollYProgress, [0, 0.8], [0.2, 1.2]);
+  const pathLengthSecond = useTransform(scrollYProgress, [0, 0.8], [0.15, 1.2]);
+  const pathLengthThird = useTransform(scrollYProgress, [0, 0.8], [0.1, 1.2]);
+  const pathLengthFourth = useTransform(scrollYProgress, [0, 0.8], [0.05, 1.2]);
+  const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
+
+  return (
+    <div
+      className="h-[400vh] w-full relative overflow-clip"
+      ref={ref}
+    >
+      <GoogleGeminiEffect
+        pathLengths={[
+          pathLengthFirst,
+          pathLengthSecond,
+          pathLengthThird,
+          pathLengthFourth,
+          pathLengthFifth,
+        ]}
+        title={"We make it happen"}
+        description={"Tailored web solutions to your unique needs"}
+        className={""}
+      />
+
+    </div>
+  );
+}
