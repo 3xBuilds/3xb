cf904efe1a2b20a54129755eb8ea8e0a2dff59f2
SayakSarkar02 <sayaksarkar73@gmail.com>
2024-02-29 16:12:33 +0530

    cards ui update

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ import Clients from '../components/Clients'
 
 const page = () => {
   return (
-    <div className='red-grad'>
+    <div className='red-grad w-screen'>
       <Home/>
 
 
-      <div className='bg-gradient-to-br from-black via-slate-900/60 to-black pt-20 border-x-0 rounded-t-3xl border-white/80 border-2'>
+      <div className='bg-gradient-to-br from-black via-slate-900/60 to-black pt-20 border-x-0 rounded-t-3xl border-white/80 border-2 w-screen'>
         <Packages/>
-        <Clients/>
+        {/* <Clients/> */}
       </div>
     </div>
   )
--- components/packages.jsx
+++ components/packages.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { PinContainer } from "../components/ui/3d-pin";
+import logo from '../assets/logo.png'
+import Image from 'next/image'
 
 const Packages = () => {
   return (
@@ -7,23 +9,26 @@ const Packages = () => {
     <h3 className='text-white text-3xl text-center mb-4'>Our Packages</h3>
         <h3 className='text-white/50 text-base text-center w-[60%] mx-auto'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit ratione alias assumenda id odio officiis debitis aliquam velit blanditiis deleniti.</h3>
         <div className='flex flex-col gap-5'>
-          <div className=" my-20 mt-20 w-full flex flex-row items-center justify-center ">
+          <div className=" my-20 mt-20 w-[85%] mx-auto grid grid-cols-3 max-md:grid-cols-1 max-md:gap-y-10 md:gap-y-20 items-center justify-center ">
             <PinContainer
               className=""
               containerClassName=""
-              title="Contact us to make your own dapp"
-              href="https://twitter.com/mannupaaji"
+              title="Contact Us to know more"
+              href="https://google.com"
             >
               <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                 <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                  Minting dapp
+                  Minting dApp: <span className='text-red-600'>$200 - $300</span> 
                 </h3>
                 <div className="text-base !m-0 !p-0 font-normal">
-                  <span className="text-slate-500 ">
-                    Dapp to Mint
+                  <span className="text-slate-500 text-sm">
+                    + $50 for native token support <br />
+                    + $100 for reveling collection
                   </span>
                 </div>
-                <div className="flex flex-1 w-full rounded-lg mt-4 red-grad brightness-150" />
+                <div className="flex flex-1 w-full rounded-lg mt-4 red-grad brightness-150 items-center justify-center">
+                  <Image alt='logo' src={logo} width={500} height={500} className=' w-40 scale-125'/>
+                </div>
               </div>
             </PinContainer>
             <PinContainer
@@ -34,14 +39,16 @@ const Packages = () => {
             >
               <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                 <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                  Minting dapp
+                  Staking dApp: <span className='text-red-600'>$400 - $500</span> 
                 </h3>
                 <div className="text-base !m-0 !p-0 font-normal">
-                  <span className="text-slate-500 ">
-                    Dapp to Mint
+                  <span className="text-slate-500 text-sm">
+                    + Rate varies depending upon custom requirements
                   </span>
                 </div>
-                <div className="flex flex-1 w-full rounded-lg mt-4 red-grad brightness-150" />
+                <div className="flex flex-1 w-full rounded-lg mt-4 red-grad brightness-150 items-center justify-center">
+                  <Image alt='logo' src={logo} width={500} height={500} className=' w-40 scale-125'/>
+                </div>
               </div>
             </PinContainer>
             <PinContainer
@@ -52,14 +59,78 @@ const Packages = () => {
             >
               <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                 <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                  Minting dapp
+                  Burning dApp: <span className='text-red-600'>$200 - $300</span> 
                 </h3>
                 <div className="text-base !m-0 !p-0 font-normal">
                   <span className="text-slate-500 ">
-                    Dapp to Mint
+                    + 150$ Trait based reward system
                   </span>
                 </div>
-                <div className="flex flex-1 w-full rounded-lg mt-4 red-grad brightness-150" />
+                <div className="flex flex-1 w-full rounded-lg mt-4 red-grad brightness-150 items-center justify-center">
+                  <Image alt='logo' src={logo} width={500} height={500} className=' w-40 scale-125'/>
+                </div>
+              </div>
+            </PinContainer>
+            <PinContainer
+              className=""
+              containerClassName=""
+              title="Contact Us to know more"
+              href="https://google.com"
+            >
+              <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+                <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+                  Raffle dApp: <span className='text-red-600'>$400 - $500</span> 
+                </h3>
+                <div className="text-base !m-0 !p-0 font-normal">
+                  <span className="text-slate-500 text-sm">
+                    + $50 for MATIC raffles <br />
+                    + $50 Unlimited Raffles
+                  </span>
+                </div>
+                <div className="flex flex-1 w-full rounded-lg mt-4 red-grad brightness-150 items-center justify-center">
+                  <Image alt='logo' src={logo} width={500} height={500} className=' w-40 scale-125'/>
+                </div>
+              </div>
+            </PinContainer>
+            <PinContainer
+              className=""
+              containerClassName=""
+              title="Contact Us to know more"
+              href="https://google.com"
+            >
+              <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+                <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+                  Marketplace: <span className='text-red-600'>$500 - $600</span> 
+                </h3>
+                <div className="text-base !m-0 !p-0 font-normal">
+                  <span className="text-slate-500 text-sm">
+                    + $100 User upload + API costs <br />
+                    + $100 Other token support
+                  </span>
+                </div>
+                <div className="flex flex-1 w-full rounded-lg mt-4 red-grad brightness-150 items-center justify-center">
+                  <Image alt='logo' src={logo} width={500} height={500} className=' w-40 scale-125'/>
+                </div>
+              </div>
+            </PinContainer>
+            <PinContainer
+              className=""
+              containerClassName=""
+              title="Contact Us to know more"
+              href="https://google.com"
+            >
+              <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+                <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+                  Airdrop dApp: <span className='text-red-600'>$200</span> 
+                </h3>
+                <div className="text-base !m-0 !p-0 font-normal">
+                  <span className="text-slate-500 text-sm">
+                  + Rate varies depending upon custom requirements
+                  </span>
+                </div>
+                <div className="flex flex-1 w-full rounded-lg mt-4 red-grad brightness-150 items-center justify-center">
+                  <Image alt='logo' src={logo} width={500} height={500} className=' w-40 scale-125'/>
+                </div>
               </div>
             </PinContainer>
           </div>
